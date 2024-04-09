e96ce22d7ded8266dd9c23fa7a585bf5920d6af1
NadiasCodes <De.nadia@outlook.com>
2024-04-09 10:37:33 +0200

    imported and used useContext among my components

---
+++ src/Components/ComponentD.jsx
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react'
+import { UserContext } from './ComponentA';
+
+
+
+const ComponentD = () => {
+  const user = useContext(UserContext);
+  return (
+    <div className='box'>
+      <h1>Component D</h1>
+      <h2>{`Bye, ${user}🧏‍♀️`}</h2>
+     
+    </div>
+  );
+}
+
+export default ComponentD
